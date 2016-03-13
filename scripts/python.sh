c44ab3259f028a5c4cba4d3fa6baf69017bf9bde
hed854 <hed854k@gmail.com>
2016-03-13 22:52:18 +0100

    Adding python yaml lib

--- scripts/python.sh
+++ scripts/python.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install python python-pip python-dev
-pip install pytest
+pip install pytest pyyaml
 
 # fabric needs python-dev
 pip install fabric
