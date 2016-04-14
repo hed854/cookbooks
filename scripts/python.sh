372d17f295ddbed6442f447b78ad9774fe297fb4
hed854 <hed854k@gmail.com>
2016-04-15 00:31:54 +0200

    Added python lib for web scraping & download

--- scripts/python.sh
+++ scripts/python.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install python python-pip python-dev
-pip install pytest pyyaml jinja2
+pip install pytest pyyaml jinja2 bs4 progressbar2
 
 # fabric needs python-dev
 pip install fabric
