b79c64bb3fef701c7b2d8edf7385a52e648bac72
hed854 <hed854k@gmail.com>
2016-05-07 01:38:40 +0200

    Installing latest version of MongoDb instead of old 2.4

--- scripts/mean.sh
+++ scripts/mean.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install nodejs npm mongodb
 
-npm install angular express mongoose nodemon
+# see https://docs.mongodb.com/v2.6/tutorial/install-mongodb-on-debian/#install-mongodb
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+apt-get update -q -y
+
+apt-get -q -y install nodejs npm mongodb-org
+
+npm install angular express mongoose grunt
+
+#npm grunt-exec
+
+# global installs
+npm install -g nodemon
 
 # Create alias for node executable
 if [[ ! -f /usr/bin/node ]]; then
