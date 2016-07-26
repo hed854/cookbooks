8520351994483120034a86af3e092e8f13e956e6
hed854 <hed854k@gmail.com>
2016-07-27 00:12:06 +0200

    Adding latest node.js version + bower in global mode

--- scripts/mean.sh
+++ scripts/mean.sh
@@ -7,16 +7,13 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 apt-get update -q -y
 
-apt-get -q -y install nodejs npm mongodb-org
+# Install latest nodejs + npm
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt-get -q -y install nodejs
+
+apt-get -q -y install mongodb-org
 
 npm install angular express mongoose grunt
 
-#npm grunt-exec
-
 # global installs
-npm install -g nodemon
-
-# Create alias for node executable
-if [[ ! -f /usr/bin/node ]]; then
-   ln -s /usr/bin/nodejs /usr/bin/node
-fi
+npm install -g nodemon bower
