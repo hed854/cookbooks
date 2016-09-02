2e3aecd0a5ec461a1d38dd99f0b9d246042d3a67
hed854 <hed854k@gmail.com>
2016-09-02 20:49:36 +0200

    Update python VM to run tdl

--- scripts/python.sh
+++ scripts/python.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install python python-pip python-dev
+apt-get -q -y install python python-pip python-dev libffi-dev
 pip install pytest pyyaml jinja2 bs4 progressbar2
 
 # fabric needs python-dev
 pip install fabric
+
+# gamedev with tdl
+# needs libffi-dev
+pip install -U cffi
+pip install libtcod-cffi
+pip install tdl
---
+++ scripts/sdl1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get -q -y install libsdl1.2-dev
